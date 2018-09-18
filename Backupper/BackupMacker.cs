781393c9ead7bdff187cd93e6e92c997167f8ad5
yegorf1 <yegorf1@gmail.com>
2018-09-18 15:34:32 +0300

    Add code (it was ignored by .gitignore :( )

---
+++ Backupper/BackupMacker.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using log4net;
+
+namespace Backupper
+{
+	/// <summary>
+	/// Class for managing backups
+	/// </summary>
+	public sealed class BackupMacker
+	{
+		private readonly List<string> _sourceDirectories;
+		private string _dateTimeFormat;
+		/// <summary>
+		/// BackupMacker's logger
+		/// </summary>
+		private ILog _logger;
+
+		/// <summary>
+		/// Directory where backups will be stored
+		/// </summary>
+		public string BackupsDirectory { get; }
+		/// <summary>
+		/// Collection of source directories
+		/// </summary>
+		public IReadOnlyCollection<string> SourceDirectories => new List<string>(_sourceDirectories);
+
+		/// <summary>
+		/// Datetime format to use in directories names
+		/// </summary>
+		public string DateTimeFormat
+		{
+			get => _dateTimeFormat;
+			set => _dateTimeFormat = value ?? throw new ArgumentNullException();
+		}
+
+		/// <summary>
+		/// Creates new BackupMacker for specified backups directory
+		/// </summary>
+		/// <param name="backupsDirectory">Directory to ave backups</param>
+		public BackupMacker(string backupsDirectory)
+		{
+			_logger = LogManager.GetLogger(typeof(BackupMacker));
+			_sourceDirectories = new List<string>();
+
+			BackupsDirectory = backupsDirectory ?? throw new ArgumentNullException(nameof(backupsDirectory));
+		}
+
+		/// <summary>
+		/// Adds source directory to list
+		/// </summary>
+		/// <param name="sourceDirectory">Directory from where to copy</param>
+		public void AddSourceDirecotry(string sourceDirectory)
+		{
+			if (sourceDirectory is null)
+			{
+				throw new ArgumentNullException(nameof(sourceDirectory));
+			}
+
+			if (_sourceDirectories.Contains(sourceDirectory))
+			{
+				throw new ArgumentException("This source directory already exists", nameof(sourceDirectory));
+			}
+
+			_sourceDirectories.Add(sourceDirectory);
+		}
+
+		/// <summary>
+		/// Removes directory from directories list
+		/// </summary>
+		/// <param name="directory">Directory to remove</param>
+		public void RemoveDirectory(string directory)
+		{
+			_sourceDirectories.Remove(directory);
+		}
+
+		/// <summary>
+		/// Makes backup at <see cref="BackupsDirectory"/> and copies all files from <see cref="SourceDirectories"/>
+		/// </summary>
+		public void MakeBackup()
+		{
+			if (_sourceDirectories.Count == 0)
+			{
+				_logger.Info("Nothing to backup");
+				return;
+			}
+
+			string destinationDirictory = Path.Combine(BackupsDirectory, DateTime.Now.ToString(DateTimeFormat));
+			_logger.Info($"Backing up to \"{destinationDirictory}\"");
+			_logger.Info($"Checking destination directory for existance and creating if it necessary..");
+
+			try
+			{
+				Directory.CreateDirectory(destinationDirictory);
+			}
+			catch (SystemException ex)
+			{
+				_logger.Error($"Unable to create destination directory due to system error. Aborting.", ex);
+				throw;
+			}
+
+			foreach (string sourceDirectory in _sourceDirectories)
+			{
+				_logger.Info($"Copying {sourceDirectory}...");
+				try
+				{
+					CopyDirectory(sourceDirectory, destinationDirictory);
+				}
+				catch (DirectoryNotFoundException ex)
+				{
+					_logger.Error($"Unable to copy {sourceDirectory}. Aborting", ex);
+					throw;
+				}
+			}
+
+			_logger.Info("Done");
+		}
+
+		/// <summary>
+		/// Recusively copies directory 
+		/// </summary>
+		/// <param name="sourceDirectoryPath">Directory to copy from</param>
+		/// <param name="destinationDirictoryPath">Destination directory</param>
+		private void CopyDirectory(string sourceDirectoryPath, string destinationDirictoryPath)
+		{
+			_logger.Debug($"Entering {sourceDirectoryPath}");
+			var sourceDirectory = new DirectoryInfo(sourceDirectoryPath);
+
+			if (!sourceDirectory.Exists)
+			{
+				throw new DirectoryNotFoundException("Source directory does not exist or could not be found.");
+			}
+
+			DirectoryInfo[] dirs = sourceDirectory.GetDirectories();
+			if (!Directory.Exists(destinationDirictoryPath))
+			{
+				_logger.Debug($"Creating {destinationDirictoryPath}...");
+				Directory.CreateDirectory(destinationDirictoryPath);
+			}
+
+			FileInfo[] files = sourceDirectory.GetFiles();
+			foreach (FileInfo file in files)
+			{
+				string temppath = Path.Combine(destinationDirictoryPath, file.Name);
+				_logger.Debug($"Copying {file.FullName}...");
+				try
+				{
+					file.CopyTo(temppath, false);
+				}
+				catch (SystemException ex)
+				{
+					_logger.Error($"Unable to copy {file.FullName}. Skipping.", ex);
+				}
+			}
+
+			foreach (DirectoryInfo subdir in dirs)
+			{
+				string temppath = Path.Combine(destinationDirictoryPath, subdir.Name);
+				CopyDirectory(subdir.FullName, temppath);
+			}
+		}
+	}
+}
---
+++ Backupper/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Filter;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using Microsoft.Extensions.Configuration;
+
+namespace Backupper
+{
+	public class Program
+	{
+		private static IConfigurationRoot Configuration { get; set; }
+		private static string DateFormat { get; set; }
+
+		private static int Main(string[] args)
+		{
+			IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(args.Length > 0 ? args[0] : "backup.json");
+
+			try
+			{
+				Configuration = configurationBuilder.Build();
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("Config file is not found");
+				return 3;
+			}
+
+			DateFormat = Configuration["datetime_fmt"] ?? "MM-dd-yyyy__HH_mm_ss";
+			SetupLogging();
+
+			ILog rootLogger = LogManager.GetLogger(typeof(Program));
+
+			if (!(Configuration["backups_directory"] is string backupsDirectory))
+			{
+				rootLogger.Error("No backups directory as specified. Aborting.");
+				return 1;
+			}
+
+			var b = new BackupMacker(backupsDirectory)
+			{
+				DateTimeFormat = DateFormat
+			};
+
+			IEnumerable<string> sourceDirectories =
+				from keyValue in Configuration.GetSection("source_directories").GetChildren()
+				select keyValue.Value;
+
+
+			foreach (string sourceDirectory in sourceDirectories)
+			{
+				if (IsSubDirectory(backupsDirectory, sourceDirectory)) {
+					rootLogger.Error($"Destination directory is inside of source directory: {sourceDirectory}");
+					return 4;
+				}
+
+				try
+				{
+					b.AddSourceDirecotry(sourceDirectory);
+				}
+				catch (ArgumentException e)
+				{
+					rootLogger.Error($"Unable to add new source directory ({sourceDirectory}): {e}");
+					return 4;
+				}
+			}
+
+			try
+			{
+				b.MakeBackup();
+			}
+			catch (Exception ex)
+			{
+				rootLogger.Error($"Unable to make backup", ex);
+				return 2;
+			}
+
+			return 0;
+		}
+
+		/// <summary>
+		/// Recursively checks is directory is inside another directory
+		/// </summary>
+		/// <param name="subdirectory">Directeory if which parent directory we will look for</param>
+		/// <param name="parentDirectory">Directory in which subdirectory could be</param>
+		/// <returns>true if <paramref name="subdirectory"/> is inside <paramref name="parentDirectory"/> otherwise false</returns>
+		private static bool IsSubDirectory(string subdirectory, string parentDirectory)
+		{
+			DirectoryInfo parent = new DirectoryInfo(parentDirectory);
+			DirectoryInfo subdir = new DirectoryInfo(subdirectory);
+
+			while (subdir.Parent != null)
+			{
+				if (subdir.Parent.FullName == parent.FullName)
+				{
+					return true;
+				}
+				else
+				{
+					subdir = subdir.Parent;
+				}
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Sets up logging to files
+		/// </summary>
+		private static void SetupLogging()
+		{
+			var hierarchy = (Hierarchy)LogManager.GetRepository(typeof(Program).Assembly);
+			var tracer = new TraceAppender();
+			var filter = new LevelMatchFilter();
+			var layout = new PatternLayout("%date{MMM/dd/yyyy HH:mm:ss,fff} [%thread] %-5level %logger %ndc – " +
+										   "%message%newline");
+
+			tracer.Layout = layout;
+			tracer.ActivateOptions();
+			hierarchy.Root.AddAppender(tracer);
+
+			var roller = new RollingFileAppender
+			{
+				File = Path.Combine(Configuration["logs_directory"] ?? "Logs", $"{DateTime.Now.ToString(DateFormat)}.log"),
+				ImmediateFlush = true,
+				AppendToFile = true,
+				StaticLogFileName = true,
+				RollingStyle = RollingFileAppender.RollingMode.Once,
+				LockingModel = new FileAppender.MinimalLock()
+			};
+			roller.AddFilter(filter);
+			roller.Layout = layout;
+			roller.ActivateOptions();
+			hierarchy.Root.AddAppender(roller);
+
+			var consoleAppender = new ConsoleAppender { Layout = layout };
+			consoleAppender.ActivateOptions();
+			hierarchy.Root.AddAppender(consoleAppender);
+
+			Level assumedLevel = hierarchy.LevelMap[Configuration["log_level"] ?? "Info"];
+			hierarchy.Root.Level = assumedLevel ?? Level.Info;
+
+			hierarchy.Configured = true;
+
+			if (assumedLevel is null)
+			{
+				LogManager.GetLogger(typeof(Program)).Error("Unable to set log level. Will use INFO.", null);
+			}
+		}
+	}
+}
